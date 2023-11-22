c726df10000987cc628dda5e77d3bcc21bef2138
majo06002 <Max-Lukas@outlook.com>
2023-11-22 10:22:09 +0100

    Add project files.

---
+++ Array 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace arrat_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] heltal = new int[3];
+            heltal[0] = 20;
+            heltal[1] = 80;
+            Console.WriteLine("Skriv ett tal:");
+            int nyttal = int.Parse(Console.ReadLine());
+            heltal[2] = nyttal;
+            Console.WriteLine(heltal[0] + " " + heltal[1] + " " + heltal[2]);
+        }
+    }
+}
